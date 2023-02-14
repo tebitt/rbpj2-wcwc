e036a8123a367334162f401949b78f8eeac02f73
tebit <tibet.tb@gmail.com>
2023-02-15 05:33:17 +0700

    Added Sockets

---
+++ client.py
@@ -0,0 +1,29 @@
+import cv2
+import socket
+from custom_socket import CustomSocket
+
+model = YOLO("best.pt")
+
+cap = cv2.VideoCapture(0)
+cap.set(3,1280)
+cap.set(4,720)
+
+host = socket.gethostname()
+port = 10101
+c = CustomSocket(host, port)
+c.clientConnect()
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        ("Camera failure")
+        continue
+    
+    msg = c.req(frame)
+
+    # cv2.imshow( "Feed", frame)
+    if cv2.waitKey(1) == ord('q'):
+        cap.release()
+    time.sleep(4)
+
+cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -1,35 +1,46 @@
-import cv2
-import time
+from custom_socket import CustomSocket
 from ultralytics import YOLO
+import socket
+import numpy as np
+import json
+import traceback
 
 model = YOLO("best.pt")
 
-current_time = 0
-cap = cv2.VideoCapture(0)
-cap.set(3,1280)
-cap.set(4,720)
+def main():
+    HOST = socket.gethostname()
+    PORT = 10101
 
-while cap.isOpened():
-    ret, frame = cap.read()
-    if not ret:
-        ("Camera failure")
-        continue
-        
-    cv2.putText(frame, str(round(1 / (time.time() - current_time), 1)) + ' fps', (25, 60),
-                cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 0), 2)
-    current_time = time.time()
-    res = model.predict(source=frame, conf=0.7, show = True)[0]
-    Detected = []
-    if res.boxes:
-        for r in res:
-            for c in res.boxes.cls:
-                Detected.append(model.names[int(c)])
+    server = CustomSocket(HOST, PORT)
+    server.startServer()
+    
+    while True:
+      
+      conn, addr = server.sock.accept()
+      print("Client connected from", addr)
+      
+      while True:
+        try:
+            detected = []
 
+            data = server.recvMsg(conn)
+            frame = np.frombuffer(data, dtype=np.uint8).reshape(720, 1280, 3)
+            res = model.predict(source=frame, conf=0.7, show = True)[0]
+            Detected = []
+            if res.boxes:
+                for r in res:
+                    for c in res.boxes.cls:
+                        Detected.append(model.names[int(c)])
 
+            print(Detected)
+            server.sendMsg(conn, json.dumps(Detected))
 
-    # cv2.imshow( "Feed", frame)
-    if cv2.waitKey(1) == ord('q'):
-        cap.release()
-    time.sleep(4)
-
-cv2.destroyAllWindows()
+        except Exception as e:
+                traceback.print_exc()
+                print(e)
+                print("Connection Closed")
+                del res
+                break
+            
+if __name__ == '__main__': 
+  main()
